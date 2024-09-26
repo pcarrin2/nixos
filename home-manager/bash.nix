024935131b6fd9fda68dc8735a7b189cc527a3bd
Pomona Carrington Hoekstra <pcarrin2@illinois.edu>
2024-09-25 21:03:58 -0500

    Update flake inputs and add an easier-on-the-eyes colorscheme for the tty

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -5,6 +5,25 @@
     historyControl = [ "ignorespace" ];
     initExtra = ''
       PS1="\a\n\[\033[1;35m\]\! \[\033[1;34m\][\u@\h:\w]\$ \[\033[0m\]"
+      if [ "$TERM" = "linux" ]; then
+				echo -en "\e]P0101010" #black
+				echo -en "\e]P82B2B2B" #darkgrey
+				echo -en "\e]P19D1515" #darkred
+				echo -en "\e]P9E33636" #red
+				echo -en "\e]P200800B" #darkgreen
+				echo -en "\e]PA8DE336" #green
+				echo -en "\e]P3E38D36" #brown
+				echo -en "\e]PBE3E336" #yellow
+				echo -en "\e]P415599E" #darkblue
+				echo -en "\e]PC348DE5" #blue
+				echo -en "\e]P59E1559" #darkmagenta
+				echo -en "\e]PDE5348D" #magenta
+				echo -en "\e]P6159E9E" #darkcyan
+				echo -en "\e]PE34E5E5" #cyan
+				echo -en "\e]P7E5E5E5" #lightgrey
+				echo -en "\e]PFFFFFFF" #white
+				clear #for background artifacting
+      fi
     '';
   };
 
