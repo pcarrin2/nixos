3e95d2a51bac6dc98566ceb32b20010361ad519f
Pomona Carrington Hoekstra <pcarrin2@illinois.edu>
2024-10-30 13:26:03 -0500

    Remove unnecessary flake inputs

--- flake.nix
+++ flake.nix
@@ -13,12 +13,6 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
-    # Doom Emacs Unstraightened
-    unstraightened = {
-      url = "github:marienz/nix-doom-emacs-unstraightened";
-      inputs.nixpkgs.follows = "";
-    };
   };
 
   outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs:
--- system/sound.nix
+++ system/sound.nix
@@ -10,9 +10,4 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-
-# patch for sound card issue.
-
-# boot.kernelPatches = [ { name = "fix-audio-1"; patch = builtins.fetchurl { url = "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/patch/sound/soc/soc-topology.c?id=e0e7bc2cbee93778c4ad7d9a792d425ffb5af6f7"; sha256 = "sha256:1y5nv1vgk73aa9hkjjd94wyd4akf07jv2znhw8jw29rj25dbab0q"; }; } { name = "fix-audio-2"; patch = builtins.fetchurl { url = "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/patch/sound/soc/soc-topology.c?id=0298f51652be47b79780833e0b63194e1231fa34"; sha256 = "sha256:14xb6nmsyxap899mg9ck65zlbkvhyi8xkq7h8bfrv4052vi414yb"; }; } ];
 }
