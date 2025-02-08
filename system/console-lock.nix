ea0db99d7f339a0d5adec36be327bd84523f3ded
Pomona Carrington Hoekstra <pcarrin2@illinois.edu>
2025-02-07 21:45:04 -0600

    mess with vlock message, update flake

--- system/console-lock.nix
+++ system/console-lock.nix
@@ -5,7 +5,7 @@
     description = "Lock physical consoles when going to sleep";
     wantedBy = ["sleep.target"];
     script = ''
-    USER=theta VLOCK_MESSAGE=$(printf "$(cat /home/theta/.config/vlock_message)") ${pkgs.vlock}/bin/vlock -an
+    USER=theta VLOCK_MESSAGE=$(/etc/nixos/system/assets/terminal-gen-graphics) ${pkgs.vlock}/bin/vlock -an
     '';
   };
 }
