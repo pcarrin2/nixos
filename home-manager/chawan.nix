1d867e2737251612b6535a22a3781ec488f57634
Pomona Carrington Hoekstra <pcarrin2@illinois.edu>
2025-07-13 23:12:06 -0500

    add chawan

---
+++ home-manager/chawan.nix
@@ -0,0 +1,19 @@
+{ ... }:
+{
+  programs.chawan = {
+    enable = true;
+    settings = {
+      buffer = {
+        images = true;
+        autofocus = true;
+        cookie = true;
+      };
+      siteconf.sr-ht = {
+          host = "lwn\.net";
+          cookie = "save";
+          scripting = true;
+      };
+      pager."C-k" = "() => pager.load('https://lite.duckduckgo.com/?=')";
+    };
+  };
+}
