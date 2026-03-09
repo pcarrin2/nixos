779205d64996a8ccfaa9f007ec04da93cee1f001
Pomona Carrington Hoekstra <pcarrin2@illinois.edu>
2026-03-09 17:59:44 -0500

    refer to nas using hostname

--- system/nfs.nix
+++ system/nfs.nix
@@ -2,7 +2,7 @@
 {
   boot.supportedFilesystems = [ "nfs" ];
   fileSystems."/mnt/nas-movies" = {
-    device = "192.168.1.46:/Movies";
+    device = "squirrel-nas.local:/Movies";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" ];
   };
